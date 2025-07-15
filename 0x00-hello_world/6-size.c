75c3513c6a97d2c2905d6be63868326a77c001cd
ndegz <ndegzmarto@gmail.com>
2025-07-15 02:47:17 -0700

    finished first introductory folder in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of a int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long: %zu byte(s)\n", sizeof(long));
+	printf("size of a long long : %zu byte(s)\n", sizeof(long long));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
